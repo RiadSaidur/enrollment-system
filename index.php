60fffa11f5bdc9d5c56984da9d13ee6f58ac0be0
Saidur Rahman Riad <reidvai1998@gmail.com>
2020-02-24 01:43:16 +0600

    updated: added name property and switched radio to select

--- index.php
+++ index.php
@@ -13,27 +13,30 @@
 <body>
   <?php include "./includes/header.php" ?>
   <?php include "./includes/toolbar.php" ?>
-  <div class="container">
+  <span class="container">
     <h1>Student Information</h1>
-    <form>
+    <form method="POST">
       <input type="number" name="reg_no" placeholder="Registration Number">
       <label for="department">
         Department:
-        <select name="department" id="department">
+        <select name="department_name" id="department">
           <option value="CSE">CSE</option>
           <option value="ECE">ECE</option>
           <option value="BBA">BBA</option>
         </select>
       </label>
-      <input type="text" name="app_name" placeholder="Student's Name">
-      <div class="gender">
-        <label for="male"><input type="radio" id="male" name="gender" value="male">Male</label>
-        <label for="female"><input type="radio" id="female" name="gender" value="female">Female</label>
-      </div>
-      <input type="number" name="gpa" placeholder="GPA">
+      <input type="text" name="std_name" placeholder="Student's Name">
+      <label for="gender">
+        Gender:
+        <select name="gender" id="gender">
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
+      </label>
+      <input type="number" name="hsc_gpa" placeholder="GPA">
       <input type="number" name="hsc_roll" placeholder="HSC Roll">
       <input type="text" name="college" placeholder="College">
-      <input type="text" name="hsc_grp" placeholder="HSC Group">
+      <input type="text" name="hsc_group" placeholder="HSC Group">
       <input type="number" name="hsc_year" placeholder="HSC Year">
       <input type="text" name="f_name" placeholder="Father's Name">
       <input type="text" name="m_name" placeholder="Mother's Name">
