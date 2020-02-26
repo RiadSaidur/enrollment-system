13174b3cbeea54dbd78ab3900650a28016af0432
Saidur Rahman Riad <reidvai1998@gmail.com>
2020-02-27 02:03:00 +0600

    updated: seperate stylesheet for header

--- index.php
+++ index.php
@@ -1,22 +1,23 @@
-<?php include './config/config.php' ?>
+<?php require_once './config/config.php' ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./index.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="./css/index.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="./css/header.css?v=<?php echo time(); ?>">
     <link rel="icon" href="./assets/house.svg">
     <title>SHAPPY | <?php print $PAGE_TITLE ?></title>
 </head>
 <body>
-  <?php include_once "./includes/header.php" ?>
-  <?php include_once "./includes/toolbar.php" ?>
+  <?php include_once "./global/header.php" ?>
+  <?php include_once "./global/toolbar.php" ?>
   <span class="container">
     <h1>Student Information</h1>
-    <?php include_once './includes/error.php' ?>
-    <?php include_once './includes/form.php' ?>
+    <?php include_once './index/error.php' ?>
+    <?php include_once './index/form.php' ?>
   </div>
-  <script src="./index.js"></script>
+  <script src="./js/index.js"></script>
 </body>
 </html>
