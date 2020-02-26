6419a70d9451e69cd61830a71aa437a4bd4650aa
Saidur Rahman Riad <reidvai1998@gmail.com>
2020-02-27 02:02:17 +0600

    updated: allstudents route

--- config/config.php
+++ config/config.php
@@ -1,24 +1,24 @@
 <?php
   switch($_SERVER['SCRIPT_NAME']) {
-    case '/customs/enrollment-system/index.php':
+    case '/index.php':
       $CURR_PAGE = "Home";
       $PAGE_TITLE = "Home";
       break;
-    case '/customs/enrollment-system/newstudent.php':
+    case '/newstudent.php':
       $CURR_PAGE = "New Student";
       $PAGE_TITLE = "Home";
       break;
-    case '/customs/enrollment-system/allstudents.php':
+    case '/allstudents.php':
       $CURR_PAGE = "All Students";
-      $PAGE_TITLE = "Student Information";
+      $PAGE_TITLE = "Student List";
       break;
-    case '/customs/enrollment-system/about.php':
+    case '/about.php':
       $CURR_PAGE = 'About';
       $PAGE_TITLE = "ABout";
       break;
     default:
-      $CURR_PAGE = "Home";
-      $PAGE_TITLE = "Home";
+      $CURR_PAGE = "";
+      $PAGE_TITLE = "Not Found";
       break;
   }
 ?>
